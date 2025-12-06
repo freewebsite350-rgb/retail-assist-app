b88d6b57f42901adae29d84b8cedacef77177390
freewebsite350-rgb <freewebsite350@gmail.com>
2025-12-06 09:51:30 +0200

    Delete app/test directory

--- app/test/page.tsx
+++
@@ -1,7 +0,0 @@
-import { supabase } from '@/lib/supabaseClient'
-
-export default async function Page() {
-  const { data, error } = await supabase.from('test').select('*')
-
-  return <pre>{JSON.stringify({ data, error }, null, 2)}</pre>
-}
